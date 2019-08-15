23efd62cd7865510fea2c9c0719e3e7448643cae
Daniel Gjorgievski <daniel.gjorgievski@rtv.de>
2019-08-15 20:29:28 +0200

    Add initial blog app

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import (
+    ListView, DetailView
+)
+
+from .models import Post
+
+class PostListView(ListView):
+    template_name = 'blog/post_list.html'
+    queryset = Post.objects.filter(
+        status=1
+    ).order_by('-created_on')
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
