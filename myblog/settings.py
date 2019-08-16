984cf7f04c76d42cc888b1d19211900605f0fd1b
gyorgium <dgjorgievski@live.com>
2019-08-16 23:25:10 +0200

    Add TODO marks

--- myblog/settings.py
+++ myblog/settings.py
@@ -121,11 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'C:/work/dev/workspace/myblogstatic/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'D:/engineering/programming/workspace/myblogstatic/static/') # TODO edit this line when goin live
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'C:/work/dev/workspace/myblogstatic/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'D:/engineering/programming/workspace/myblogstatic/media/') # TODO edit this line when goin live
