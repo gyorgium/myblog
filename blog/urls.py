169631112a9f8f3ce1da4336026beb5c03d45991
gyorgium <dgjorgievski@live.com>
2019-08-21 00:06:52 +0200

    add new views

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,16 @@ from . import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='home'),
+    path('about/', views.AboutView.as_view(), name='about'),
     path('post/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('posts/', views.AllPostListView.as_view(), name='all_post_list'),
+    path('post/create/', views.CreatePostView.as_view(), name='post_create'),
+    path('post/<slug:slug>/edit/', views.UpdatePostView.as_view(), name='post_update'),
+    path('post/<slug:slug>/delete/', views.DeletePostView.as_view(), name='post_delete'),
+    path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+    path('post/<slug:slug>/publish/', views.publish_post(), name='post_publish'),
+    path('post/<slug:slug>/comment/', views.add_comment_to_post(), name='add_post_comment'),
+    path('comment/<slug:slug>/approve/', views.approve_comment, name='comment_approve'),
+    path('comment/<slug:slug>/remove/', views.remove_comment, name='comment_remove'),
     path('summernote/', include('django_summernote.urls')),
 ]
 
