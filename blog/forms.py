5618b89cbbb77972813b1408ccd10d585a2e8b09
gyorgium <dgjorgievski@live.com>
2019-08-21 14:03:33 +0200

    add mandatory field slug to the post form

--- blog/forms.py
+++ blog/forms.py
@@ -5,7 +5,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('author','title', 'content',)
+        fields = ('author','title', 'slug', 'content',)
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'textinputclass'}),
