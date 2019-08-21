d5dea5ebd44595c80cfd2af723234ac0e2a235ea
gyorgium <dgjorgievski@live.com>
2019-08-21 23:21:31 +0200

    change home into post_list

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='home'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('about/', views.AboutView.as_view(), name='about'),
     path('post/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
     path('create/post/', views.CreatePostView.as_view(), name='post_create'),
